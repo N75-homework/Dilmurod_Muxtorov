f4a74a08ae9cdd663c9986fbb2a4fecbf2758005
RealistD <dilmurodsenator@gmail.com>
2022-11-09 10:12:51 +0500

    block-6

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,65 +1,46 @@
-var grant = 'Zor endi siz Talabasiz';
-var kantrakt = 'Koproq dars qilish kerak edi kantraktga kirdingiz';
-var yiqilgan = 'Siz tanlagan OTM ga oqishga tavsiya etilmadiz (yana 1yil sabir sizga)';
-var error = 'Siz notgri malumot kiritdingiz (maktabni karidorda bitirdizmi)';
-var xato = 'Buncha bal yoq lekin siz toplagansz qoyil (maktabni karidorda bitirdizmi)';
-var firstName = prompt('Ismingizni kiriting');
+var firstName = prompt('Ismingizni kiriting:');
+var lastName = prompt('Familiyangizni kiriting:');
+var number = prompt('Nomeringizni kiriting:');
+var email = prompt('Emailingizni kiriting:');
+var address = prompt('Addressingizni kiriting');
+var scholl = prompt('Nechinchi maktabda oqigansiz');
+var schollNumber = prompt('Maktabingizni qaysi yillarda oqigansiz');
+var Unversy = prompt('Qaysi OTM da talim olgansiz');
+var unversyNumber = prompt('OTM ni qaysi yillarda oqigansiz');
+var work = prompt('Ayni vaqtda qayerda ishlaysiz');
 
+main.innerHTML = `
+<div class="image"><img src="../assets/icons/Screenshot 2022-11-09 094503.png"></div>
+<div>
+  <h1 class="firstName" id="firstName">${firstName}</h1>
+  <h1 class="lastName" id="lastName">${lastName}</h1>
+  <div class="item"> 
+    <h3>Phone:</h3>
+    <ins class="number" id="number">${+number}</ins>
+  </div>
+  <div class="input">
+    <div>
+      <h3>Email:</h3>
+      <p id="email">${email}</p>
+    </div>
+    <div>
+      <h3>Addres:</h3>
+      <p id="address">${address}</p>  
+    </div>
+  </div>
+</div>
+<div>
+  <h3>Education</h3>
+  <ins class="schollNumber" id="schoolNumber">${schoolNumber}</ins>
+  <p class="scholl" id="scholl"> ${scholl}</p> 
+  <br>
+  <ins class="unversyNumber" id="unversyNumber">${unversyNumber}</ins>
+  <p class="Unversy" id="Unversy">${Unversy}</p>
+</div>
+<div>
+  <h3>Experience</h3>
+  <p class="work" id="work">${work}</p> 
+</div>
 
-var OTM = prompt(firstName + ' OTM lar dan birini tanlang: 1.TDIU  2.TDMI  3.MU');
-
-var ball = prompt( firstName + ' balingizni kiriting:');
-
-if (OTM == 1){   
-    if(ball >= 150){
-        alert( grant);
-    }
-    else if( ball > 120){
-        alert(kantrakt);
-    }
-    else if(120 >= ball > 0){
-        alert( yiqilgan);
-    }
-    
-    if ( 189 < ball) {
-    alert(xato)
-    }
-    else{
-        alert(error);
-    }
-}else if(OTM == 2){
-    if(ball >= 160){
-        alert(grant);
-    }
-    if ( 189 < ball) {
-        alert(xato)
-        }
-    else if(ball > 100){
-        alert(kantrakt);
-    }
-    else if(100 >= ball > 0){
-        alert(yiqilgan);
-    }
-    else{
-        alert(error);
-    }
-}else if(OTM == 3){
-    if(ball >= 180){
-        alert(grant);
-    }
-    if ( 189 < ball) {
-        alert(xato)
-        }
-    else if( ball > 168){
-        alert(kantrakt);
-    }
-    else if(168 >= ball > 0){
-        alert(yiqilgan);
-    }
-    else{
-        alert(error);
-    }
-}else{
-    alert(error);
-}
+`
 
