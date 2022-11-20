faf0a87baaa6eaf01cc46c9259936b812671326e
RealistD <dilmurodsenator@gmail.com>
2022-11-20 19:19:01 +0500

    Block-9

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,67 @@
-let outputScreen = document.getElementById("output-screen");
+// let data = [
+//     {
+//         image: `../assets/icons/car-1.svg`
 
+//     },
+//     {
+//         image: `../assets/icons/car-2.svg`
+//     },
+//     {
+//         image: `../assets/icons/car-3.svg`
 
-function display(num) {
-  outputScreen.value += num;
-}
+//     },
+//     {
+//         image: `../assets/icons/car-4.svg`
 
-
-function Calculate() {
-  try {
-    outputScreen.value = eval(outputScreen.value);
-  }
-  catch (err) {
-    alert("O'ylab yozing 😊")
-  }
-
-
-
-
-}
-function Clear() {
-  outputScreen.value = "";
-}
+//     },
+// ]
 
 
 
-function del() {
-  outputScreen.value = outputScreen.value.slice(0, -1);
+// let $tabBtn = document.querySelectorAll('.tabBtn')
+// let $tabContent = document.getElementById('tabContent')
 
-}
+
+// $tabContent.innerHTML = `
+// <div>
+// <img src="${data[0].image}">
+// </div>`
+
+
+
+// for (let i = 0; i < $tabBtn.length; i++) {
+
+//     $tabBtn[i].addEventListener('click', () => {
+//         let htmlContent = `
+//      <div>
+//      <img width="400" src="${data[i].image}">
+//      </div>`
+
+//         $tabContent.innerHTML = htmlContent
+//     })
+
+
+// }
+const swiper = new Swiper('.swiper',{
+    navigation
+});
+let btn1 = document.querySelector('.tabBtn1'),
+    btn2 = document.querySelector('.tabBtn2'),
+    btn3 = document.querySelector('.tabBtn3'),
+    btn4 = document.querySelector('.tabBtn4');
+
+btn1.addEventListener('click', (e) => {
+   
+    swiper.slideTo(0, 210)
+})
+btn2.addEventListener('click', (e) => {
+
+    swiper.slideTo(1, 210)
+})
+btn3.addEventListener('click', (e) => {
+
+    swiper.slideTo(2, 210)
+})
+btn4.addEventListener('click', (e) => {
+    swiper.slideTo(3, 210)
+});
