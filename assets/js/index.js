1c6c766d0b03e66c5e9a805fbef3cc48a4bf497d
RealistD <dilmurodsenator@gmail.com>
2022-11-21 12:49:22 +0500

    block--11

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,10 +14,6 @@ function makeItRain() {
 
 makeItRain()
 
-
-
-
-
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
